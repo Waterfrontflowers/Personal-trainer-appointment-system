788aa09abbbb5b270be117222b6819fd48ea9336
Ginger <739865919@qq.com>
2022-09-08 14:37:27 +0800

    Package error correction

--- src/main/java/com/nchu/ptas/component/AddResponseHeaderFilter.java
+++ src/main/java/com/nchu/ptas/component/AddResponseHeaderFilter.java
@@ -1,4 +1,4 @@
-package com.harmony.card.component;
+package com.nchu.ptas.component;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
